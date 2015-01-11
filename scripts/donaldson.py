013de3e9887c6d1f65efc602bf1f35709942099d
Abe <abe@braxas>
2015-01-11 14:47:22 -0800

    Update donaldson module to use alias function

--- scripts/donaldson.py
+++ scripts/donaldson.py
@@ -1,22 +1,16 @@
-import requests
 from bs4 import BeautifulSoup
-import sys
+from crossfilter.common.util import get, format_brand
 
 
 def getFilter(filterNumber, brand, full=False):
-	brand = brand.upper()
-	filterNumber = filterNumber.upper()
-
-	if brand == 'ACDELCO':
-		brand = 'AC'
-	if brand == 'MOBIL':
-		brand = 'MOBIL OIL'
-
+    brand_names, filterNumber = format_brand('donaldson',
+                                             brand,
+                                             filterNumber)
 	address = 'http://www.michelecaroli.com/filter_cross_reference.asp?tpr=S2&f1=%s'
 	address = address % filterNumber
 
-	page = requests.get(address)
-	soup = BeautifulSoup(page.text)
+	content = get(address)
+	soup = BeautifulSoup(content)
 
 	form = soup.find(id='forminvio')
 	donaldsons = set()
@@ -44,7 +38,7 @@ def getFilter(filterNumber, brand, full=False):
 					print ', '.join([cell.getText().strip() for cell in cells])
 				match_brand = cells[1].get_text().strip()
 				match_brand = match_brand.strip('\n')
-				if match_brand == brand:
+				if match_brand in brand_names:
 					num = cells[0].get_text().strip()
 					num = num.strip('\n')
 					if(num == filterNumber):
